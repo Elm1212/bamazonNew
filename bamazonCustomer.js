e8155c301099f01e1815808637c9f5a1a3f2bd4f
Elm <emmal.muniz@gmail.com>
2019-01-17 19:54:26 -0600

    fixed item id bug

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,7 +19,7 @@ connection.connect(function(err){
 var makeTable = function(){
     connection.query("SELECT * FROM products", function(err,res){
         for(var i=0; i<res.length; i++){
-            console.log(res[i].itemid+" || "+res[i].product_name+" || "+res[i].department_name+" || "+res[i].price+" || "+res[i].stock_quantity+"\n");
+            console.log(res[i].item_id+" || "+res[i].product_name+" || "+res[i].department_name+" || "+res[i].price+" || "+res[i].stock_quantity+"\n");
         }
     promptCustomer(res);
     })
